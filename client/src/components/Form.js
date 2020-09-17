b8772f59733a3e72d54d01349e30be0f38285553
HoJin9622 <kiss0104040@gmail.com>
2020-09-17 23:47:51 +0900

    feat: Leads 추가 구현

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addLead } from "../actions/leads";
 
 const Form = () => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const lead = {
+      name,
+      email,
+      message,
+    };
+
+    dispatch(addLead(lead));
+  };
+
   return (
     <div>
-      <h1>Add Lead Form</h1>
+      <h2>Add Lead</h2>
+
+      <form onSubmit={onSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
+
+        <label>Emaill</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+
+        <label>Message</label>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.currentTarget.value)}
+        />
+        <button type="submit" onClick={onSubmit}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
